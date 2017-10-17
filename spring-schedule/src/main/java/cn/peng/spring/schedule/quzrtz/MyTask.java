8e22a1d2f4b2a7bde21d368f8075e397d4af2e56
peng <308979657@qq.com>
2017-10-18 00:01:45 +0800

    Spring配置QuartZ

--- spring-schedule/src/main/java/cn/peng/spring/schedule/quzrtz/MyTask.java
+++ spring-schedule/src/main/java/cn/peng/spring/schedule/quzrtz/MyTask.java
@@ -2,15 +2,12 @@ package cn.peng.spring.schedule.quzrtz;
 
 import java.text.SimpleDateFormat;
 
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.scheduling.quartz.QuartzJobBean;
 
 //定义一个专门负责任务处理的程序类
-public class MyTask extends QuartzJobBean {
+public class MyTask {
 	
-	@Override
-	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+	
+	protected void runTack() {//自己定义了一个方法名称
 		System.out.println("当前日期时间"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS").format(new java.util.Date()));
 
 	}
